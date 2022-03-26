12ed0503729a9670c081e909e648a1b87861f211
vmrfreitas <vmrfreitas@inf.ufrgs.br>
2022-03-26 01:08:48 -0300

    player movement kinda

--- isometric-stuff/Assets/Scripts/PlayerMovement.cs
+++ isometric-stuff/Assets/Scripts/PlayerMovement.cs
@@ -2,19 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    
+    private Rigidbody2D playerRigidBody;
+    private float moveHorizontal, moveVertical;
+    [SerializeField] private float moveSpeed = 1.0f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        moveHorizontal = Input.GetAxis("Horizontal") * moveSpeed;
+        moveVertical = Input.GetAxis("Vertical")* moveSpeed;
+        playerRigidBody.velocity = new Vector2(moveHorizontal, moveVertical);
     }
 }
