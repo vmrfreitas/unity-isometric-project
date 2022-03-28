2fafa9a5648d00cd9197e964d3b746b6a0e6bd90
vmrfreitas <vmrfreitas@inf.ufrgs.br>
2022-03-28 02:19:00 -0300

    still thinking about it, not working

--- isometric-stuff/Assets/Scripts/PlayerMovement.cs
+++ isometric-stuff/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -7,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D playerRigidBody;
     private float moveHorizontal, moveVertical;
     [SerializeField] private float moveSpeed = 1.0f;
+    [SerializeField] private Grid grid;
     
     // Start is called before the first frame update
     void Start()
@@ -18,12 +18,30 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 currentPos = playerRigidBody.position;
+        Vector3Int gridCurrPos = grid.WorldToCell((Vector3)playerRigidBody.position);
+        int roundedHorizontalInput, roundedVerticalInput;
+        Debug.Log(gridCurrPos);
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector2 inputVector = new Vector2(horizontalInput,verticalInput);
-        inputVector = Vector2.ClampMagnitude(inputVector, 1);
 
-        Vector2 movement = inputVector * moveSpeed;
+        if(horizontalInput>0){
+            roundedHorizontalInput = (int)Math.Ceiling(horizontalInput);
+        } else {
+            roundedHorizontalInput = (int)Math.Floor(horizontalInput);
+        }
+
+        if(verticalInput>0){
+            roundedVerticalInput = (int)Math.Ceiling(horizontalInput);
+        } else {
+            roundedVerticalInput = (int)Math.Floor(horizontalInput);
+        }
+        Vector3Int gridNewPos = gridCurrPos + new Vector3Int(roundedHorizontalInput, roundedVerticalInput, 0);
+
+        inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        float moveForce = inputVector.magnitude;
+
+        Vector2 movement = moveForce * (Vector2)grid.CellToWorld(gridNewPos) * moveSpeed;
         Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
 
         playerRigidBody.MovePosition(newPos);
